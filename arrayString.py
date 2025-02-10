45bb312572aa5cff675d2db1399bf7035883c372
Chetanya2001 <mathurchetanya1@gmail.com>
2025-02-10 16:56:13 +0530

    array string done

---
+++ arrayString.py
@@ -0,0 +1,27 @@
+# One D array
+arr = [1,2,3,"name"]
+print(arr[0])
+print(arr[1])
+print(arr[2])
+print(arr[3])
+print(arr)
+
+# Two D array
+Two_D_array = [[1,2,3],
+               [4,5,6],
+               [7,8,9]]
+print(Two_D_array[0][0])
+print(Two_D_array[0][1])
+print(Two_D_array[0][2])
+print(Two_D_array[1][0])
+print(Two_D_array[1][1])
+print(Two_D_array[1][2])
+print(Two_D_array[2][0])
+print(Two_D_array[2][1])
+print(Two_D_array[2][2])
+print(Two_D_array)
+
+#String 
+name = "chetanya"
+print(name[1:3])
+print(name[0:4])
