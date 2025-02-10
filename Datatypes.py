49f17c171dff5274c42934d4cf73646a07ceae41
Chetanya2001 <mathurchetanya1@gmail.com>
2025-02-10 16:07:18 +0530

    datatypes

---
+++ Datatypes.py
@@ -0,0 +1,32 @@
+# Numeric type
+num = 5
+deci = 2.3
+# Sequence Type
+word = "chetanya"
+numbers = [1,2,3,4] #mutable
+tuples = (1,2,3) #immutable
+count = range(1,10) #immutable
+# Set types
+x = {1,2,3} #unordered and unique elements and mutable
+y = ({1,2,3})
+#Mapping type
+dic = {
+    "name" : "chetanya",
+    "age" : 27
+}
+#Boolean Type
+a = 5 > 2
+#Null type
+b = None
+
+print(type(num))
+print(type(deci))
+print(type(word))
+print(type(numbers))
+print(type(tuples))
+print(type(count))
+print(type(x))
+print(type(y))
+print(type(dic))
+print(type(a))
+print(type(b))
